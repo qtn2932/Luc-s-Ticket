06267e997b812573648f6cc78c5617864d101f6b
qtn2932 <quan.nguyen199712@gmail.com>
2019-02-28 15:07:10 -0500

    added fire config

---
+++ ticket/src/components/config/fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+firebase.initializeApp({
+    apiKey: "AIzaSyBdRKb8kNtutXC_pKfe-oV63b_tgDBrlH8",
+    authDomain: "luc-s-ticket.firebaseapp.com",
+    databaseURL: "https://luc-s-ticket.firebaseio.com",
+    projectId: "luc-s-ticket",
+    storageBucket: "luc-s-ticket.appspot.com",
+    messagingSenderId: "881283862238"
+})
+const fire= firebase
+export default fire
